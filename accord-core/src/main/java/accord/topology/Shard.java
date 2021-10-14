d1cb16ac85feea83a356df84bae1ee3ab1771bbf
Blake Eggleston <bdeggleston@gmail.com>
2021-10-14 11:18:19 -0700

    add pending nodes to shards

--- accord-core/src/main/java/accord/topology/Shard.java
+++ accord-core/src/main/java/accord/topology/Shard.java
@@ -1,5 +1,6 @@
 package accord.topology;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 
@@ -10,6 +11,7 @@ import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Iterables;
 
 public class Shard
 {
@@ -18,21 +20,30 @@ public class Shard
     public final List<Id> nodes;
     public final Set<Id> nodeSet;
     public final Set<Id> fastPathElectorate;
+    public final Set<Id> pending;
     public final int recoveryFastPathSize;
     public final int fastPathQuorumSize;
     public final int slowPathQuorumSize;
 
-    public Shard(KeyRange range, List<Id> nodes, Set<Id> fastPathElectorate)
+    public Shard(KeyRange range, List<Id> nodes, Set<Id> fastPathElectorate, Set<Id> pending)
     {
+        Preconditions.checkArgument(Iterables.all(pending, nodes::contains),
+                                    "pending nodes must also be present in nodes");
         this.range = range;
         this.nodes = ImmutableList.copyOf(nodes);
         this.nodeSet = ImmutableSet.copyOf(nodes);
         int f = maxToleratedFailures(nodes.size());
         this.fastPathElectorate = ImmutableSet.copyOf(fastPathElectorate);
+        this.pending = ImmutableSet.copyOf(pending);
         int e = fastPathElectorate.size();
         this.recoveryFastPathSize = (f+1)/2;
         this.slowPathQuorumSize = f + 1;
         this.fastPathQuorumSize = fastPathQuorumSize(nodes.size(), e, f);
+    }
+
+    public Shard(KeyRange range, List<Id> nodes, Set<Id> fastPathElectorate)
+    {
+        this(range, nodes, fastPathElectorate, Collections.emptySet());
     }
 
     @VisibleForTesting
--- accord-core/src/test/java/accord/topology/ShardTest.java
+++ accord-core/src/test/java/accord/topology/ShardTest.java
@@ -1,7 +1,15 @@
 package accord.topology;
 
+import accord.local.Node;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import static accord.Utils.ids;
+import static accord.impl.IntKey.range;
 
 public class ShardTest
 {
@@ -44,19 +52,25 @@ public class ShardTest
         Assertions.assertEquals(expected, actual);
     }
 
-    void assertInvalidFastPathElectorateSize(int replicas, int fpElectorate)
+    void assertIllegalArgument(Runnable runnable, String msg)
     {
-        int f = Shard.maxToleratedFailures(replicas);
         try
         {
-            Shard.fastPathQuorumSize(replicas, fpElectorate, f);
-            Assertions.fail(String.format("Expected exception for fp electorate size %s for replica set size %s (f %s)",
-                                          fpElectorate, replicas, f));
+            runnable.run();
+            Assertions.fail(msg);
         }
         catch (IllegalArgumentException e)
         {
             // noop
         }
+    }
+
+    void assertInvalidFastPathElectorateSize(int replicas, int fpElectorate)
+    {
+        int f = Shard.maxToleratedFailures(replicas);
+        assertIllegalArgument(() -> Shard.fastPathQuorumSize(replicas, fpElectorate, f),
+                              String.format("Expected exception for fp electorate size %s for replica set size %s (f %s)",
+                                            fpElectorate, replicas, f));
     }
 
     @Test
@@ -104,5 +118,17 @@ public class ShardTest
         assertFastPathQuorumSize(6, 9, 6);
         assertFastPathQuorumSize(5, 9, 5);
         assertInvalidFastPathElectorateSize(9, 4);
+    }
+
+    @Test
+    void pendingNodeValidation()
+    {
+        List<Node.Id> nodes = ids(0, 3);
+        Set<Node.Id> fpNodes = new HashSet<>(ids(0, 2));
+        // pending nodes are part of electorate
+        new Shard(range(0, 100), nodes, fpNodes, new HashSet<>(ids(3, 3)));
+        // pending nodes are not part of electorate
+        assertIllegalArgument(() -> new Shard(range(0, 100), nodes, fpNodes, new HashSet<>(ids(4, 4))),
+                              "Expected exception for non-electorate pending nodes");
     }
 }
