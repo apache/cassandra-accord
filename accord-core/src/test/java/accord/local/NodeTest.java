5070cbe29e41f71f3d91fa299d3277fc7fc95872
Blake Eggleston <bdeggleston@gmail.com>
2021-11-10 15:44:17 -0800

    fix tests

--- accord-core/src/test/java/accord/local/NodeTest.java
+++ accord-core/src/test/java/accord/local/NodeTest.java
@@ -1,6 +1,7 @@
 package accord.local;
 
 import accord.impl.mock.MockCluster;
+import accord.impl.mock.MockConfigurationService;
 import accord.txn.Timestamp;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -16,59 +17,66 @@ public class NodeTest
     void uniqueNowTest()
     {
         MockCluster.Clock clock = new MockCluster.Clock(100);
-        MockCluster cluster = MockCluster.builder().nowSupplier(clock).build();
-        Node node = cluster.get(1);
+        try (MockCluster cluster = MockCluster.builder().nowSupplier(clock).build())
+        {
+            Node node = cluster.get(1);
 
-        clock.increment();
-        Timestamp timestamp1 = node.uniqueNow();
-        Timestamp timestamp2 = node.uniqueNow();
+            clock.increment();
+            Timestamp timestamp1 = node.uniqueNow();
+            Timestamp timestamp2 = node.uniqueNow();
 
-        clock.increment();
-        Timestamp timestamp3 = node.uniqueNow();
+            clock.increment();
+            Timestamp timestamp3 = node.uniqueNow();
 
-        Assertions.assertEquals(ts(1, 101, 0, 1), timestamp1);
-        Assertions.assertEquals(ts(1, 101, 1, 1), timestamp2);
+            Assertions.assertEquals(ts(1, 101, 0, 1), timestamp1);
+            Assertions.assertEquals(ts(1, 101, 1, 1), timestamp2);
 
-        Assertions.assertEquals(ts(1, 102, 0, 1), timestamp3);
+            Assertions.assertEquals(ts(1, 102, 0, 1), timestamp3);
+        }
     }
 
     @Test
     void uniqueNowEpochUpdate()
     {
         MockCluster.Clock clock = new MockCluster.Clock(100);
-        MockCluster cluster = MockCluster.builder().nowSupplier(clock).build();
-        Node node = cluster.get(1);
+        try (MockCluster cluster = MockCluster.builder().nowSupplier(clock).build())
+        {
+            Node node = cluster.get(1);
+            MockConfigurationService configService = (MockConfigurationService) node.configService();
 
-        clock.increment();
-        Timestamp timestamp1 = node.uniqueNow();
-        Assertions.assertEquals(ts(1, 101, 0, 1), timestamp1);
+            clock.increment();
+            Timestamp timestamp1 = node.uniqueNow();
+            Assertions.assertEquals(ts(1, 101, 0, 1), timestamp1);
 
-        node.onTopologyUpdate(node.topology().current().withEpoch(2));
-        Timestamp timestamp2 = node.uniqueNow();
-        Assertions.assertEquals(ts(2, 101, 1, 1), timestamp2);
+            configService.reportTopology(node.topology().current().withEpoch(2));
+            Timestamp timestamp2 = node.uniqueNow();
+            Assertions.assertEquals(ts(2, 101, 1, 1), timestamp2);
+        }
     }
 
     @Test
     void uniqueNowAtLeastTest()
     {
         MockCluster.Clock clock = new MockCluster.Clock(100);
-        MockCluster cluster = MockCluster.builder().nowSupplier(clock).build();
-        Node node = cluster.get(1);
+        try (MockCluster cluster = MockCluster.builder().nowSupplier(clock).build())
+        {
+            Node node = cluster.get(1);
 
-        clock.increment();
-        Timestamp timestamp1 = node.uniqueNow();
-        Assertions.assertEquals(ts(1, 101, 0, 1), timestamp1);
+            clock.increment();
+            Timestamp timestamp1 = node.uniqueNow();
+            Assertions.assertEquals(ts(1, 101, 0, 1), timestamp1);
 
-        // atLeast equal to most recent ts, logical should increment
-        Assertions.assertEquals(ts(1, 101, 1, 1),
-                                node.uniqueNow(timestamp1));
+            // atLeast equal to most recent ts, logical should increment
+            Assertions.assertEquals(ts(1, 101, 1, 1),
+                                    node.uniqueNow(timestamp1));
 
-        // atLeast less than most recent ts
-        Assertions.assertEquals(ts(1, 101, 2, 1),
-                                node.uniqueNow(ts(1, 99, 0, 1)));
+            // atLeast less than most recent ts
+            Assertions.assertEquals(ts(1, 101, 2, 1),
+                                    node.uniqueNow(ts(1, 99, 0, 1)));
 
-        // atLeast greater than most recent ts
-        Assertions.assertEquals(ts(1, 110, 1, 1),
-                                node.uniqueNow(ts(1, 110, 0, 2)));
+            // atLeast greater than most recent ts
+            Assertions.assertEquals(ts(1, 110, 1, 1),
+                                    node.uniqueNow(ts(1, 110, 0, 2)));
+        }
     }
 }
