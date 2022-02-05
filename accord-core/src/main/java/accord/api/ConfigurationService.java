9f89990ecfd68bd8212e97cc3efe167ad93f8b53
Blake Eggleston <bdeggleston@gmail.com>
2022-02-04 16:38:54 -0800

    improve ConfigurationService docs

--- accord-core/src/main/java/accord/api/ConfigurationService.java
+++ accord-core/src/main/java/accord/api/ConfigurationService.java
@@ -7,8 +7,34 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
 
 /**
- * Cluster configuration service. Manages linearizable cluster configuration changes. Any node reporting a
- * configuration with epoch n is guaranteed to have previously seen and applied every previous epoch configuration
+ * ConfigurationService is responsible for:
+ *  - coordinating linearizable configuration / topology changes across the cluster
+ *  - notifying the local node of topology changes
+ *  - syncing transaction metadata between replicas on epoch changes
+ *  - updating the fast path electorate when appropriate
+ *  - acting as a database of previous epoch configurations
+ *
+ * The high level flow of a topology change is as follows:
+ *
+ *  - ConfigurationService becomes aware of a new epoch/Topology
+ *
+ *  - ConfigurationService notifies the node of the new configuration service by calling
+ *      {@link accord.api.ConfigurationService.Listener#onTopologyUpdate(accord.topology.Topology)}
+ *
+ *  - Once the node has setup the new topology, it will call {@link accord.api.ConfigurationService#acknowledgeEpoch(long)}
+ *      which indicates the node will no longer create txnIds for the previous epoch, and it's commits can now be synced
+ *      with other replicas.
+ *
+ *  - ConfigurationService is now expected to begin syncing fast path decisions made in the previous epoch to other
+ *      replicas. In the case of a replication change (new replica, ownership movement), the configuration service
+ *      needs to notify the new replica of the transaction history of the relevant ranges.
+ *
+ *  - Once the previous epoch data has been synced to other nodes, the configuration service needs to notify the other
+ *      nodes that this node has synced data for the previous epoch.
+ *
+ *  - ConfigurationService will notify the node when other nodes complete syncing an epoch by calling
+ *      {@link accord.api.ConfigurationService.Listener#onEpochSyncComplete(accord.local.Node.Id, long)}
+ *
  */
 public interface ConfigurationService
 {
