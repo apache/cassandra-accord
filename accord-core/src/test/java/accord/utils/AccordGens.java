03363735d81c38677c1a1095a76e4cc7a60db541
David Capwell <dcapwell@gmail.com>
2023-03-10 18:22:22 -0800

    fixed bug where RangeDepBuilder tries to sort on range without comparator

---
+++ accord-core/src/test/java/accord/utils/AccordGens.java
@@ -0,0 +1,154 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package accord.utils;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+import java.util.function.BiFunction;
+
+import accord.api.Key;
+import accord.api.RoutingKey;
+import accord.impl.IntHashKey;
+import accord.impl.IntKey;
+import accord.local.Node;
+import accord.primitives.Deps;
+import accord.primitives.KeyDeps;
+import accord.primitives.Range;
+import accord.primitives.RangeDeps;
+import accord.primitives.Routable;
+import accord.primitives.Timestamp;
+import accord.primitives.Txn;
+import accord.primitives.TxnId;
+
+public class AccordGens
+{
+    public static Gen.LongGen epochs()
+    {
+        return Gens.longs().between(0, Timestamp.MAX_EPOCH);
+    }
+
+    public static Gen<TxnId> txnIds()
+    {
+        return txnIds(epochs()::nextLong, RandomSource::nextLong, RandomSource::nextInt);
+    }
+
+    public static Gen<TxnId> txnIds(Gen.LongGen epochs, Gen.LongGen hlcs, Gen.IntGen nodes)
+    {
+        Gen<Txn.Kind> kinds = Gens.enums().all(Txn.Kind.class);
+        Gen<Routable.Domain> domains = Gens.enums().all(Routable.Domain.class);
+        return rs -> new TxnId(epochs.nextLong(rs), hlcs.nextLong(rs), kinds.next(rs), domains.next(rs), new Node.Id(nodes.nextInt(rs)));
+    }
+
+    public static Gen<Key> intKeys()
+    {
+        return rs -> new IntKey.Raw(rs.nextInt());
+    }
+
+    public static Gen<Key> intHashKeys()
+    {
+        return rs -> IntHashKey.key(rs.nextInt());
+    }
+
+    public static Gen<KeyDeps> keyDeps(Gen<Key> keyGen)
+    {
+        return keyDeps(keyGen, txnIds());
+    }
+
+    public static Gen<KeyDeps> keyDeps(Gen<Key> keyGen, Gen<TxnId> idGen)
+    {
+        double emptyProb = .2D;
+        return rs -> {
+            if (rs.decide(emptyProb)) return KeyDeps.NONE;
+            Set<Key> seenKeys = new HashSet<>();
+            Set<TxnId> seenTxn = new HashSet<>();
+            Gen<Key> uniqKeyGen = keyGen.filter(seenKeys::add);
+            Gen<TxnId> uniqIdGen = idGen.filter(seenTxn::add);
+            try (KeyDeps.Builder builder = KeyDeps.builder())
+            {
+                for (int i = 0, numKeys = rs.nextInt(1, 10); i < numKeys; i++)
+                {
+                    builder.nextKey(uniqKeyGen.next(rs));
+                    seenTxn.clear();
+                    for (int j = 0, numTxn = rs.nextInt(1, 10); j < numTxn; j++)
+                        builder.add(uniqIdGen.next(rs));
+                }
+                return builder.build();
+            }
+        };
+    }
+
+    public interface RangeFactory
+    {
+        Range create(RandomSource rs, RoutingKey a, RoutingKey b);
+    }
+
+    public static Gen<Range> ranges(Gen<RoutingKey> keyGen, BiFunction<? super RoutingKey, ? super RoutingKey, ? extends Range> factory)
+    {
+        return ranges(keyGen, (ignore, a, b) -> factory.apply(a, b));
+    }
+
+    public static Gen<Range> ranges(Gen<RoutingKey> keyGen)
+    {
+        return ranges(keyGen, (rs, a, b) -> {
+            boolean left = rs.nextBoolean();
+            return Range.range(a, b, left, !left);
+        });
+    }
+
+    public static Gen<Range> ranges(Gen<RoutingKey> keyGen, RangeFactory factory)
+    {
+        RoutingKey[] keys = new RoutingKey[2];
+        return rs -> {
+            keys[0] = keyGen.next(rs);
+            // range doesn't allow a=b
+            do keys[1] = keyGen.next(rs);
+            while (Objects.equals(keys[0], keys[1]));
+            Arrays.sort(keys);
+            return factory.create(rs, keys[0], keys[1]);
+        };
+    }
+
+    public static Gen<RangeDeps> rangeDeps(Gen<Range> rangeGen)
+    {
+        return rangeDeps(rangeGen, txnIds());
+    }
+
+    public static Gen<RangeDeps> rangeDeps(Gen<Range> rangeGen, Gen<TxnId> idGen)
+    {
+        double emptyProb = .2D;
+        return rs -> {
+            if (rs.decide(emptyProb)) return RangeDeps.NONE;
+            RangeDeps.Builder builder = RangeDeps.builder();
+            for (int i = 0, numKeys = rs.nextInt(1, 10); i < numKeys; i++)
+            {
+                builder.nextKey(rangeGen.next(rs));
+                for (int j = 0, numTxn = rs.nextInt(1, 10); j < numTxn; j++)
+                    builder.add(idGen.next(rs));
+            }
+            return builder.build();
+        };
+    }
+
+    public static Gen<Deps> deps(Gen<KeyDeps> keyDepsGen, Gen<RangeDeps> rangeDepsGen)
+    {
+        return rs -> new Deps(keyDepsGen.next(rs), rangeDepsGen.next(rs));
+    }
+}
--- accord-maelstrom/src/test/java/accord/maelstrom/JsonTest.java
+++ accord-maelstrom/src/test/java/accord/maelstrom/JsonTest.java
@@ -18,29 +18,15 @@
 
 package accord.maelstrom;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Objects;
-import java.util.Set;
-import java.util.function.ToIntFunction;
-import java.util.function.ToLongFunction;
-
 import org.junit.jupiter.api.Test;
 
 import accord.api.Key;
-import accord.api.RoutingKey;
-import accord.local.Node;
 import accord.primitives.Deps;
-import accord.primitives.KeyDeps;
 import accord.primitives.Range;
 import accord.primitives.RangeDeps;
-import accord.primitives.Routable;
-import accord.primitives.Timestamp;
-import accord.primitives.Txn;
-import accord.primitives.TxnId;
+import accord.utils.AccordGens;
 import accord.utils.Gen;
 import accord.utils.Gens;
-import accord.utils.RandomSource;
 import org.assertj.core.api.Assertions;
 
 import static accord.utils.Property.qt;
@@ -69,23 +55,6 @@ class JsonTest
                   .isEqualTo(expected);
     }
 
-    public static Gen.LongGen epochs()
-    {
-        return Gens.longs().between(0, Timestamp.MAX_EPOCH);
-    }
-
-    public static Gen<TxnId> ids()
-    {
-        return ids(epochs()::nextLong, RandomSource::nextLong, RandomSource::nextInt);
-    }
-
-    public static Gen<TxnId> ids(ToLongFunction<RandomSource> epochs, ToLongFunction<RandomSource> hlcs, ToIntFunction<RandomSource> nodes)
-    {
-        Gen<Txn.Kind> kinds = Gens.enums().all(Txn.Kind.class);
-        Gen<Routable.Domain> domains = Gens.enums().all(Routable.Domain.class);
-        return rs -> new TxnId(epochs.applyAsLong(rs), hlcs.applyAsLong(rs), kinds.next(rs), domains.next(rs), new Node.Id(nodes.applyAsInt(rs)));
-    }
-
     static Gen<Key> keyGen()
     {
         Gen<Datum.Kind> kindGen = Gens.enums().all(Datum.Kind.class);
@@ -101,88 +70,21 @@ class JsonTest
                 default: throw new AssertionError("Unknown kind: " + next);
             }
         };
-//        return rs -> new IntKey.Raw(rs.nextInt());
     }
 
-    static Gen<KeyDeps> keyDepsGen()
-    {
-        Gen<TxnId> idGen = ids();
-        Gen<Key> keyGen = keyGen();
-        double emptyProb = .2D;
-        return rs -> {
-            if (rs.decide(emptyProb)) return KeyDeps.NONE;
-            Set<Key> seenKeys = new HashSet<>();
-            Set<TxnId> seenTxn = new HashSet<>();
-            Gen<Key> uniqKeyGen = keyGen.filter(seenKeys::add);
-            Gen<TxnId> uniqIdGen = idGen.filter(seenTxn::add);
-            try (KeyDeps.Builder builder = KeyDeps.builder())
-            {
-                for (int i = 0, numKeys = rs.nextInt(1, 10); i < numKeys; i++)
-                {
-                    builder.nextKey(uniqKeyGen.next(rs));
-                    seenTxn.clear();
-                    for (int j = 0, numTxn = rs.nextInt(1, 10); j < numTxn; j++)
-                        builder.add(uniqIdGen.next(rs));
-                }
-                return builder.build();
-            }
-        };
-    }
-
-//    static Gen<Range> rangeGen()
-//    {
-//        Gen<RoutingKey> keyGen = keyGen().map(Key::toUnseekable);
-//        RoutingKey[] keys = new RoutingKey[2];
-//        return rs -> {
-//            keys[0] = keyGen.next(rs);
-//            // range doesn't allow a=b
-//            do keys[1] = keyGen.next(rs);
-//            while (Objects.equals(keys[0], keys[1]));
-//            Arrays.sort(keys);
-//            boolean left = rs.nextBoolean();
-//            return Range.range(keys[0], keys[1], left, !left);
-//        };
-//    }
     static Gen<Range> rangeGen()
     {
-        Gen<RoutingKey> keyGen = keyGen().map(Key::toUnseekable);
-        RoutingKey[] keys = new RoutingKey[2];
-        return rs -> {
-            keys[0] = keyGen.next(rs);
-            // range doesn't allow a=b
-            do
-            {
-                keys[1] = keyGen.next(rs);
-            }
-            while (Objects.equals(keys[0], keys[1]));
-            Arrays.sort(keys);
-            return new MaelstromKey.Range(keys[0], keys[1]);
-        };
+        return AccordGens.ranges(keyGen().map(Key::toUnseekable), MaelstromKey.Range::new);
     }
 
     static Gen<RangeDeps> rangeDepsGen()
     {
-        return Gens.constant(RangeDeps.NONE);
-//        Gen<TxnId> idGen = ids();
-//        Gen<Range> rangeGen = rangeGen();
-//        double emptyProb = .2D;
-//        return rs -> {
-//            if (rs.decide(emptyProb)) return RangeDeps.NONE;
-//            RangeDeps.Builder builder = RangeDeps.builder();
-//            for (int i = 0, numKeys = rs.nextInt(1, 10); i < numKeys; i++)
-//            {
-//                builder.nextKey(rangeGen.next(rs));
-//                for (int j = 0, numTxn = rs.nextInt(1, 10); j < numTxn; j++)
-//                    builder.add(idGen.next(rs));
-//            }
-//            return builder.build();
-//        };
+        return AccordGens.rangeDeps(rangeGen());
     }
 
     static Gen<Deps> depsGen()
     {
-        Gen<KeyDeps> keyDepsGen = keyDepsGen();
-        Gen<RangeDeps> rangeDepsGen = rangeDepsGen();
-        return rs -> new Deps(keyDepsGen.next(rs), rangeDepsGen.next(rs));
+        return AccordGens.deps(AccordGens.keyDeps(keyGen()), rangeDepsGen());
     }
+
 }
