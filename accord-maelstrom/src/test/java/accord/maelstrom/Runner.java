8f31b3e1741d413be2d43d64bea2618e3c4ba525
David Capwell <dcapwell@gmail.com>
2023-03-10 17:31:51 -0800

    Fixed bug where accord.maelstrom.Runner.StandardQueue#queue was accessed from multiple threads without locking

--- accord-maelstrom/src/test/java/accord/maelstrom/Runner.java
+++ accord-maelstrom/src/test/java/accord/maelstrom/Runner.java
@@ -18,15 +18,16 @@
 
 package accord.maelstrom;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.PriorityQueue;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicLong;
-import java.util.function.Function;
 import java.util.function.Supplier;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import accord.local.Node.Id;
 import accord.maelstrom.Cluster.Queue;
@@ -38,6 +39,8 @@ import static accord.utils.Utils.toArray;
 
 public class Runner
 {
+    private static final Logger logger = LoggerFactory.getLogger(Runner.class);
+
     static class StandardQueue<T> implements Queue<T>
     {
         static class Factory implements QueueSupplier
@@ -89,19 +92,19 @@ public class Runner
         }
 
         @Override
-        public void add(T item)
+        public synchronized void add(T item)
         {
             add(item, random.nextInt(500), TimeUnit.MILLISECONDS);
         }
 
         @Override
-        public void add(T item, long delay, TimeUnit units)
+        public synchronized void add(T item, long delay, TimeUnit units)
         {
             queue.add(new Item<>(now + units.toMillis(delay), seq++, item));
         }
 
         @Override
-        public T poll()
+        public synchronized T poll()
         {
             Item<T> item = queue.poll();
             if (item == null)
@@ -111,189 +114,10 @@ public class Runner
         }
 
         @Override
-        public int size()
+        public synchronized int size()
         {
             return queue.size();
         }
-    }
-
-    static class RandomQueue<T> implements Queue<T>
-    {
-        static class Factory implements QueueSupplier
-        {
-            final RandomSource seeds;
-
-            Factory(RandomSource seeds)
-            {
-                this.seeds = seeds;
-            }
-
-            @Override
-            public <T> Queue<T> get()
-            {
-                return new RandomQueue<>(seeds.fork());
-            }
-        }
-
-        static class Entry<T> implements Comparable<Entry<T>>
-        {
-            final double priority;
-            final T value;
-
-            Entry(double priority, T value)
-            {
-                this.priority = priority;
-                this.value = value;
-            }
-
-            @Override
-            public int compareTo(Entry<T> that)
-            {
-                return Double.compare(this.priority, that.priority);
-            }
-        }
-
-        final PriorityQueue<Entry<T>> queue = new PriorityQueue<>();
-        final RandomSource random;
-
-        public RandomQueue(RandomSource random)
-        {
-            this.random = random;
-        }
-
-        @Override
-        public int size()
-        {
-            return queue.size();
-        }
-
-        @Override
-        public void add(T item)
-        {
-            queue.add(new Entry<>(random.nextDouble(), item));
-        }
-
-        @Override
-        public void add(T item, long delay, TimeUnit units)
-        {
-            queue.add(new Entry<>(random.nextDouble(), item));
-        }
-
-        @Override
-        public T poll()
-        {
-            return unwrap(queue.poll());
-        }
-
-        private static <T> T unwrap(Entry<T> e)
-        {
-            return e == null ? null : e.value;
-        }
-    }
-
-    static <T> Supplier<T> parseOutput(boolean delay, String output, Function<String, T> parse)
-    {
-        return parseOutput(delay, output.split("\n"), parse);
-    }
-
-    static <T> Supplier<T> parseOutput(boolean delay, BufferedReader output, Function<String, T> parse)
-    {
-        return parseOutput(delay, output.lines().toArray(String[]::new), parse);
-    }
-
-    static <T> Supplier<T> parseOutput(boolean delay, String[] output, Function<String, T> parse)
-    {
-        long[] nanos = new long[output.length];
-        String[] commands = new String[output.length];
-
-        for (int i = 0 ; i < output.length ; ++i)
-        {
-            String command = output[i];
-            long at = TimeUnit.MILLISECONDS.toNanos(Long.parseLong(command.substring(0, command.indexOf(' '))));
-            command = command.substring(command.indexOf(' ') + 1);
-            if (i > 0 && at <= nanos[i-1]) at = nanos[i-1] + 1;
-            nanos[i] = at;
-            commands[i] = command;
-        }
-
-        long start = System.nanoTime();
-        return new Supplier<T>()
-        {
-            int i = 0;
-            @Override
-            public T get()
-            {
-                if (i == commands.length)
-                    return null;
-
-                while (delay)
-                {
-                    long wait = start + nanos[i] - System.nanoTime();
-                    if (wait <= 0) break;
-                    try
-                    {
-                        TimeUnit.NANOSECONDS.sleep(wait);
-                    }
-                    catch (InterruptedException e)
-                    {
-                        throw new IllegalStateException(e);
-                    }
-                }
-
-                return parse.apply(commands[i++]);
-            }
-        };
-    }
-
-    static void parseNode(TopologyFactory factory, boolean delay, String output) throws IOException
-    {
-        Main.listen(factory, parseOutput(delay, output, Function.identity()), System.out, System.err);
-    }
-
-    // TODO (low priority, maelstrom): we need to align response ids with the input; for now replies are broken
-    static void replay(int nodeCount, TopologyFactory factory, boolean delay, Supplier<Packet> input) throws IOException
-    {
-        run(nodeCount, new QueueSupplier()
-        {
-            @Override
-            public <T> Queue<T> get()
-            {
-                return new Queue<T>()
-                {
-                    @Override
-                    public void add(T t)
-                    {
-                    }
-
-                    @Override
-                    public void add(T item, long delay, TimeUnit units)
-                    {
-                    }
-
-                    @Override
-                    public T poll()
-                    {
-                        return (T)input.get();
-                    }
-
-                    @Override
-                    public int size()
-                    {
-                        return 0;
-                    }
-                };
-            }
-        }, DefaultRandom::new, factory, () -> null);
-    }
-
-    static void run(TopologyFactory factory, String ... commands) throws IOException
-    {
-        run(3, factory, commands);
-    }
-
-    static void run(int nodeCount, TopologyFactory factory, String ... commands) throws IOException
-    {
-        run(nodeCount, new StandardQueue.Factory(new DefaultRandom()), DefaultRandom::new, factory, commands);
     }
 
     static void run(int nodeCount, QueueSupplier queueSupplier, Supplier<RandomSource> randomSupplier, TopologyFactory factory, String ... commands) throws IOException
@@ -316,5 +140,52 @@ public class Runner
             nodes.add(new Id(i));
 
         Cluster.run(toArray(nodes, Id[]::new), queueSupplier, ignore -> {}, randomSupplier, () -> new AtomicLong()::incrementAndGet, factory, commands, System.err);
+    }
+
+    public static Builder test()
+    {
+        return new Builder(Thread.currentThread().getStackTrace()[2].getMethodName());
+    }
+
+    public static class Builder
+    {
+        public static final String ACCORD_TEST_SEED = "accord.test.%s.seed";
+
+        private final String key;
+        private long seed;
+        private int nodeCount = 3;
+        private TopologyFactory factory = new TopologyFactory(4, 3);
+
+        private Builder(String name)
+        {
+            key = String.format(ACCORD_TEST_SEED, name);
+            String userSeed = System.getProperty(key, null);
+            seed = userSeed != null ? Long.parseLong(userSeed) : System.nanoTime();
+        }
+
+        Builder seed(long seed)
+        {
+            this.seed = seed;
+            return this;
+        }
+
+        Builder nodeCount(int nodeCount)
+        {
+            this.nodeCount = nodeCount;
+            return this;
+        }
+
+        Builder factory(TopologyFactory factory)
+        {
+            this.factory = factory;
+            return this;
+        }
+
+        void run(String ... commands) throws IOException
+        {
+            logger.info("Seed {}; rerun with -D{}={}", seed, key, seed);
+            RandomSource randomSource = new DefaultRandom(seed);
+            Runner.run(nodeCount, new StandardQueue.Factory(randomSource), randomSource::fork, factory, commands);
+        }
     }
 }
