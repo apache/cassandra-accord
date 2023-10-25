39eda98d5770572172f4ce76010c14df361b9b91
David Capwell <dcapwell@gmail.com>
2023-10-25 15:06:43 -0700

    include jdk that return 8 rather than 1.8

--- accord-core/src/test/java/accord/burn/BurnTest.java
+++ accord-core/src/test/java/accord/burn/BurnTest.java
@@ -351,7 +351,7 @@ public class BurnTest
     private static Verifier createVerifier(int keyCount)
     {
         int jdkVersion = Integer.parseInt(StandardSystemProperty.JAVA_VERSION.value().split("\\.")[0]);
-        if (jdkVersion == 1) // 1.8
+        if (jdkVersion == 1 /* 1.8 */ || jdkVersion == 8)
             // Elle only works on JDK 11
             return new StrictSerializabilityVerifier(keyCount);
         return CompositeVerifier.create(new StrictSerializabilityVerifier(keyCount),
