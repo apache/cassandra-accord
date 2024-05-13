192fc930e1de267691aa4bf46178196905a68dc5
David Capwell <dcapwell@apache.org>
2024-05-13 14:16:26 -0700

    feedback

--- accord-core/src/main/java/accord/local/Command.java
+++ accord-core/src/main/java/accord/local/Command.java
@@ -1699,6 +1699,7 @@ public abstract class Command implements CommonAttributes
     static Command.Accepted acceptInvalidated(Command command, Ballot ballot)
     {
         SaveStatus saveStatus = SaveStatus.get(Status.AcceptedInvalidate, command.known());
+        // TODO (desired): This should be NonNull, but AcceptedInvalidated is represented by Command.Accepted because there’s no acceptedOrCommitted register in NotDefined
         return validate(new Command.Accepted(command, saveStatus, ballot, command.executeAt(), command.partialTxn(), null, ballot));
     }
 
