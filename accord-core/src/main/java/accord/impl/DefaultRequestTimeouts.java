78badf67faf811ae5ee3e709e28f9b52223784ec
Alex Petrov <oleksandr.petrov@gmail.com>
2024-10-01 10:25:54 +0200

    Pull in Ariel's fix

--- accord-core/src/main/java/accord/impl/DefaultRequestTimeouts.java
+++ accord-core/src/main/java/accord/impl/DefaultRequestTimeouts.java
@@ -18,6 +18,8 @@
 
 package accord.impl;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.ReentrantLock;
 import java.util.function.Function;
@@ -91,16 +93,19 @@ public class DefaultRequestTimeouts implements RequestTimeouts
         @Override
         public void run()
         {
+            List<Timeout> timedOut = new ArrayList<>();
             lock.lock();
             try
             {
                 long now = node.elapsed(MILLISECONDS);
                 // TODO (expected): should we handle reentrancy? Or at least throw an exception?
-                timeouts.advance(now, this, (s, r) -> r.timeout.timeout());
+                timeouts.advance(now, this, (s, r) -> timedOut.add(r.timeout));
             }
             finally
             {
                 lock.unlock();
+                for (Timeout timeout : timedOut)
+                    timeout.timeout();
             }
         }
 
