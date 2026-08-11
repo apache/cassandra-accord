6c95fe61a1422cebf2a89e1c56aba511c26d634e
Alan Wang <alanwang599@gmail.com>
2026-08-11 12:27:50 -0700

    fix

--- accord-core/src/test/java/accord/utils/SortedArraysTest.java
+++ accord-core/src/test/java/accord/utils/SortedArraysTest.java
@@ -241,9 +241,9 @@ class SortedArraysTest
             int[] expected = intersection.stream().mapToInt(i->i).toArray();
             Arrays.sort(expected);
 
-            // First condition covers the case where we are expected to return the exact copy
+            // Condition covers the case where we are expected to return the exact copy
             // if the intersection is an exact match of the smaller array
-            if (bStart == 0 && bEnd == bInt.length && bInt.length == bSet.size() && aSet.containsAll(bSet))
+            if (bStart == 0 && bEnd == bInt.length && bInt.length == bSet.size() && ((bEnd - bStart) != (aEnd - aStart)) && aSet.containsAll(bSet))
             {
                 Assertions.assertSame(bInt, SortedArrays.linearIntersection(aInt, aStart, a.length, bInt, bStart, bInt.length, new ArrayBuffers.IntBufferCache(4, 1 << 14)));
                 Assertions.assertSame(bInt, SortedArrays.linearIntersection(bInt, bStart, bInt.length, aInt, aStart, a.length, new ArrayBuffers.IntBufferCache(4, 1 << 14)));
