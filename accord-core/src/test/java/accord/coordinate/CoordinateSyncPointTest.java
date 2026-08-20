745536797ca6cb980a8d15d9891130a54ab2b2cc
Alan Wang <alanwang599@gmail.com>
2026-08-20 12:49:12 -0700

    fix

--- accord-core/src/test/java/accord/coordinate/CoordinateSyncPointTest.java
+++ accord-core/src/test/java/accord/coordinate/CoordinateSyncPointTest.java
@@ -70,7 +70,8 @@ class CoordinateSyncPointTest
                                    Utils.shard(IntKey.range(0, 10), ALL),
                                    Utils.shard(removed, new SortedArrayList<>(new Node.Id[] { N2 })));
 
-        Node n1 = Utils.createNode(N1, t1, happyPathMessaging(), new MockCluster.Clock(0), new TestAgent.RethrowAgent());
+        MockCluster.Clock clock = new MockCluster.Clock(0);
+        Node n1 = Utils.createNode(N1, t1, happyPathMessaging(), clock, new TestAgent.RethrowAgent(clock));
         n1.topology().reportTopology(t2);
         for (Node.Id node : ALL)
             n1.topology().onReadyToCoordinate(node, t1.epoch());
@@ -86,16 +87,19 @@ class CoordinateSyncPointTest
         Topology t2 = new Topology(t1.epoch() + 1,
                                    Utils.shard(IntKey.range(0, 10), ALL));
 
-        Node n1 = Utils.createNode(N1, t1, happyPathMessaging(), new MockCluster.Clock(0), new TestAgent.RethrowAgent());
+        MockCluster.Clock clock = new MockCluster.Clock(0);
+        Node n1 = Utils.createNode(N1, t1, happyPathMessaging(), clock, new TestAgent.RethrowAgent(clock));
         n1.topology().reportTopology(t2);
         for (Node.Id node : ALL)
             n1.topology().onReadyToCoordinate(node, t1.epoch());
 
         awaitApplied(n1, removed);
 
+        clock.increment();
         n1.topology().onEpochRetired(Ranges.single(removed), t2.epoch());
         awaitApplied(n1, removed);
 
+        clock.increment();
         n1.topology().onEpochClosed(Ranges.single(removed), t2.epoch());
         awaitApplied(n1, removed);
     }
